37969e2818bb54ebf6e384a34dd980926db61b9e
Ben Brown <Ben_Brown19@pal-nat186-188-195.itap.purdue.edu>
2023-02-01 09:26:55 -0500

    initial script for cloning to local

---
+++ cloner.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python3
+import sys
+import git
+from git import Repo
+
+def clone_repo(url):
+    f = open(url, "r")
+    url_string = f.readline().rstrip('\n')
+    print(url_string) 
+    Repo.clone_from(url_string, "/Users/Ben_Brown19/Desktop/school/ECE_461/cloneage/zlone")
+    #repo = Repo.clone_from("https://github.com/brow1770/goprac.git", "/Users/Ben_Brown19/Desktop/school/ECE_461/cloneage/blone")
+    f.close()
+
+def main():
+    repo_url = sys.argv[1]
+    clone_repo(repo_url)    
+
+if __name__ == "__main__":
+    main()
