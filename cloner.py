a0a651715177d68bb2312fbd3c2a0c4c6bb5ed14
Ben Brown <Ben_Brown19@Bens-MacBook-Pro.local>
2023-02-11 23:50:06 -0500

    added npm source compatability

--- cloner.py
+++ cloner.py
@@ -5,13 +5,9 @@ import subprocess
 from git import Repo
 
 def clone_repo(url, dir_path):
-    #f = open(url, "r")
-    #url_string = f.readline().rstrip('\n')
-    #print(url_string)
-    Repo.clone_from(str(url), "/Users/Ben_Brown19/Desktop/school/ECE_461/461-Project/cloneDir/g" + str(dir_path))
-    #subp = subprocess.Popen([])
-    #subp = subprocess.call(["cd", "/Users/Ben_Brown19/Desktop/school/ECE_461/461-Project/cloneDir/g" + str(dir_path), "&&", "git", "rev-list", "--all", "--count"], shell= True, cwd= "/Users/Ben_Brown19/Desktop/school/ECE_461/461-Project")
-    subp = subprocess.run(["git rev-list --all --count"], stdout=subprocess.PIPE, text=True, shell=True, cwd= "/Users/Ben_Brown19/Desktop/school/ECE_461/461-Project/cloneDir/g" + str(dir_path))
+
+    Repo.clone_from(str(url), "/Users/Ben_Brown19/Desktop/school/ECE_461/461-Project/cloneDir/" + str(dir_path))
+    subp = subprocess.run(["git rev-list --all --count"], stdout=subprocess.PIPE, text=True, shell=True, cwd= "/Users/Ben_Brown19/Desktop/school/ECE_461/461-Project/cloneDir/" + str(dir_path))
     print(subp.stdout)
     
 
--- run.go
+++ run.go
@@ -11,6 +11,8 @@ import (
 	"path/filepath"
 	"strings"
 	"strconv"
+	"encoding/binary"
+	"math"
 )
 
 type attribute struct{
@@ -23,10 +25,31 @@ type attribute struct{
 	license string 
 }
 
-func newURL(url string) *attribute{
+type gitObject struct{
+	numCommits float32
+	numPullRequests float32
+	graphQL float32
+}
+
+type npmObject struct{
+	numCommits float32
+	numMaintainers float32
+	graphQL float32
+	gitRepo string
+}
+
+func newURL(url string) (*attribute) {
 	scoreObject := attribute{url: url}
 	return &scoreObject
 }
+/*func newNpmObject(url string) (*npmObject) {
+	npmObj := npmObject{}
+	return &npmObj
+}
+func newGitObject(url string) (*gitObject) {
+	gitObj := {url: url}
+	return &npmObj
+}*/
 
 func installDeps() {
 	file, err := os.Open("requirements.txt")
@@ -93,8 +116,7 @@ func file(filename string) {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-	var githubCount int = 0
-	var	npmjsCount int = 0
+	var	urlCount int = 0
 	for scanner.Scan() {
 		line := scanner.Text()
 		//fmt.Println(line)
@@ -102,11 +124,13 @@ func file(filename string) {
 
 		// call sub function to score each line/project
 		if strings.Contains(line, "github.com") {
-			githubCount += 1
-			github(line, scoreObject, githubCount)
+			//var gitObj gitObject
+			urlCount += 1
+			github(line, scoreObject, urlCount)
 		} else if strings.Contains(line, "npmjs.com") {
-			npmjsCount += 1
-			npmjs(line, scoreObject, npmjsCount)
+			var npmObj npmObject
+			urlCount += 1
+			npmjs(line, scoreObject, urlCount, &npmObj)
 		} else {
 			fmt.Println("Error: ", line, "is not a valid URL")
 		}
@@ -119,21 +143,23 @@ func github(url string, scoreObject *attribute, count int) {
 	repo := split[len(split)-1]
 	print("Owner: ", owner, " Repo: ", repo, "\n")
 	value1 := githubSource(scoreObject, url, count)
-	var fullRepo string = owner + "/" + repo
-	value2 := githubPullReq(fullRepo, scoreObject, url)
+	//var fullRepo string = owner + "/" + repo
+	//value2 := githubPullReq(fullRepo, scoreObject, url)
 	//value3 := githubGraphQL
 	//intConv, _ := strconv.Atoi(string(value))
 	fmt.Println(value1)
-	fmt.Println(value2)
+	//fmt.Println(value2)
 }
 
-func npmjs(url string, scoreObject *attribute, count int) {
+func npmjs(url string, scoreObject *attribute, count int, npmObj *npmObject) {
 	split := strings.Split(url, "/")
 	packageName := split[len(split)-1]
 	print("Package: ", packageName, "\n")
-	npmRestAPI(packageName, scoreObject)
+	npmRestAPI(packageName, scoreObject, npmObj)
 	//npmGraphQL
-	//npmSource
+	npmSource(npmObj, count)
+	fmt.Println(npmObj.gitRepo)
+	fmt.Println(npmObj.numCommits)
 }
 
 func githubSource(scoreObject *attribute, url string, count int) (output []byte){
@@ -148,6 +174,22 @@ func githubSource(scoreObject *attribute, url string, count int) (output []byte)
 	}
 	
 	return output
+
+}
+
+func npmSource(npmObj *npmObject, count int) {
+
+	//call python script that clones repo and pull number of commits
+	command := exec.Command("python3", "cloner.py", "https://github.com/" + npmObj.gitRepo, strconv.Itoa(count))
+	output, err := command.Output()
+
+	if err != nil{
+		fmt.Println(err.Error())
+		return
+	}
+	b := binary.BigEndian.Uint32(output)
+	float := math.Float32frombits(b)
+	npmObj.numCommits = float
 
 }
 
@@ -176,7 +218,7 @@ func githubPullReq(repoName string, scoreObject *attribute, url string) (value2 
 	//fmt.Printf("Number of pull requests in joelchiang2k/Linkr: %d\n", pullRequests.TotalCount)
 }
 
-func npmRestAPI(packageName string, scoreObject *attribute) {
+func npmRestAPI(packageName string, scoreObject *attribute, npmObj *npmObject) {
 	
 	//append packageName to the api url and send request
 	url := "https://registry.npmjs.org/" + packageName
@@ -219,6 +261,14 @@ func npmRestAPI(packageName string, scoreObject *attribute) {
 	//output numContributors and license
 	fmt.Print("number of contributors: ", scoreObject.responsiveness)
 	fmt.Print("\nlicense: ", contributors["license"].(string))
+	//fmt.Print("\ngithub url: ", contributors["repository"].(map[string]interface{})["url"])
+	split := strings.Split(contributors["repository"].(map[string]interface{})["url"].(string), "/")
+	owner := split[len(split)-2]
+	repo := split[len(split)-1]
+	fmt.Println("Owner: " ,owner, " Repo: ", repo)
+	//var npmGitString string = owner + "/" + repo
+	npmObj.gitRepo = owner + "/" + repo
+	//fmt.Print("\ngithub url: ", contributors["repository"].(map[string]interface{})["url"])
 	fmt.Print("\n")
 
 }
